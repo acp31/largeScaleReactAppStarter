44dd95e5bb51f9c193e6459bf5f7d71e406c144b
Aaron Phillips <aphillips@johnmcneiltudio.com>
2017-03-28 15:32:34 -0700

    added react-router to project

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -3,11 +3,22 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return (
+        <li key={song.id} className="collection-item">
+          {song.title}
+        </li>
+      );
+    });
+  }
+
   render() {
+    if(this.props.data.loading) { return <div>Loading...</div>; }
     return(
-      <div>
-        SongList
-      </div>
+      <ul className="collection">
+        {this.renderSongs()}
+      </ul>
     );
   }
 }
@@ -15,6 +26,7 @@ class SongList extends Component {
 const query = gql`
   {
     songs {
+      id
       title
     }
   }
