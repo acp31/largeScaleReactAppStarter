dfc6cf5d86b92d8fd0d320a152555a77f8f42897
Aaron Phillips <aphillips@johnmcneiltudio.com>
2017-04-17 12:10:03 -0700

    new changes

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = 'mongodb://Aaron:Northwest99@ds159988.mlab.com:59988/lyricaldb';
+const MONGO_URI = '';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
