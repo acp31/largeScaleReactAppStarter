aad8d1456d49c4d78d76b56796fd35ff5b3ca112
Aaron Phillips <aphillips@johnmcneiltudio.com>
2017-04-17 13:26:24 -0700

    added linking to song detail page

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
   render() {
+
+    const { song } = this.props.data;
+
+    if(!song) { return <div></div>; }
+
     return (
       <div>
-        <h3>Song Detail</h3>
+        <Link to="/">Back</Link>
+        <h3>{song.title}</h3>
       </div>
     );
   }
 }
 
-export default SongDetail;
+export default graphql(fetchSong, {
+  options: (props) => { return { variables: { id: props.params.id } } }
+})(SongDetail);
