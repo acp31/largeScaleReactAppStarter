07022e72022bbd064651184c027e9ccb09c386d3
Aaron Phillips <aphillips@johnmcneiltudio.com>
2017-04-17 12:51:19 -0700

    added delete and fetch song mutation

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import { Link, hashHistory } from 'react-router';
-import query from '../queries/fetchSongs'
+import query from '../queries/fetchSongs';
 
 class SongCreate extends Component {
   constructor(props){
---
+++ client/components/SongDetail.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import fetchSong from '../queries/fetchSong';
+
+class SongDetail extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Song Detail</h3>
+      </div>
+    );
+  }
+}
+
+export default SongDetail;
